afe49347f183adc5be0d25d928209e6244c2ddb8
sowakarol <karosowa@student.agh.edu.pl>
2018-10-25 09:19:45 +0200

    Added new View Controller and scene

---
+++ wetter/AddCityViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AddCityViewController.swift
+//  wetter
+//
+//  Created by Student on 25.10.2018.
+//  Copyright © 2018 karl. All rights reserved.
+//
+
+import UIKit
+
+class AddCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func AddButton(_ sender: Any) {
+    }
+    
+        @objc
+        func insertNewObject(_ sender: Any) {
+//            objects.insert(NSDate(), at: 0)
+//            let indexPath = IndexPath(row: 0, section: 0)
+//            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- wetter/MasterViewController.swift
+++ wetter/MasterViewController.swift
@@ -19,8 +19,8 @@ class MasterViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = editButtonItem
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
+//        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+//        navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -32,12 +32,12 @@ class MasterViewController: UITableViewController {
         super.viewWillAppear(animated)
     }
 
-    @objc
-    func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
-    }
+//    @objc
+//    func insertNewObject(_ sender: Any) {
+//        objects.insert(NSDate(), at: 0)
+//        let indexPath = IndexPath(row: 0, section: 0)
+//        tableView.insertRows(at: [indexPath], with: .automatic)
+//    }
 
     // MARK: - Segues
 
